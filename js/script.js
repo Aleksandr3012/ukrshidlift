dd7bdb4a998e45b0a0cbd0e066daa329ac59665f
Aleksandr3012 <flaming541@gmail.com>
2019-04-09 15:08:11 +0300

    3

---
+++ js/script.js
@@ -0,0 +1,46 @@
+
+
+//
+// $(document).ready(function(){
+//
+//
+//
+//     $('.owl-carousel').owlCarousel({
+//
+//         navText: [
+//             ' ',
+//             ' '],
+//
+//         loop:true,
+//         margin:0,
+//         nav:true,
+//
+//         responsive:{
+//             0:{
+//                 items:1
+//             },
+//             600:{
+//                 items:1
+//             },
+//             1000:{
+//                 items:1
+//             }
+//         }
+//     });
+// });
+
+$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 500);
+    });
+});
