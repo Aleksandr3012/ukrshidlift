a17fda68cfbe54fcb25db93f8a3c8069e8231276
Aleksandr3012 <flaming541@gmail.com>
2019-04-14 23:57:26 +0300

    index.html pre done

--- js/script.js
+++ js/script.js
@@ -1,41 +1,12 @@
+// якоря
 
-
-//
-// $(document).ready(function(){
-//
-//
-//
-//     $('.owl-carousel').owlCarousel({
-//
-//         navText: [
-//             ' ',
-//             ' '],
-//
-//         loop:true,
-//         margin:0,
-//         nav:true,
-//
-//         responsive:{
-//             0:{
-//                 items:1
-//             },
-//             600:{
-//                 items:1
-//             },
-//             1000:{
-//                 items:1
-//             }
-//         }
-//     });
-// });
-
-$(document).ready(function(){
-    $("#menu").on("click","a", function (event) {
+$(document).ready(function () {
+    $("#form").on("click", "a", function (event) {
         //отменяем стандартную обработку нажатия по ссылке
         event.preventDefault();
 
         //забираем идентификатор бока с атрибута href
-        var id  = $(this).attr('href'),
+        var id = $(this).attr('href'),
 
             //узнаем высоту от начала страницы до блока на который ссылается якорь
             top = $(id).offset().top;
@@ -44,3 +15,56 @@ $(document).ready(function(){
         $('body,html').animate({scrollTop: top}, 500);
     });
 });
+
+
+// прилипание шапки
+var h_hght = 141; // высота шапки
+var h_mrg = 0;    // отступ когда шапка уже не видна
+
+$(function () {
+
+    var elem = $('.header-menu');
+    var top = $(this).scrollTop();
+
+    if (top > h_hght) {
+        elem.css('top', h_mrg);
+    }
+
+    $(window).scroll(function () {
+        top = $(this).scrollTop();
+
+        if (top + h_mrg < h_hght) {
+            elem.css('top', (h_hght - top));
+        } else {
+            elem.css('top', h_mrg);
+        }
+    });
+
+});
+
+
+// карусель на фотогалерею
+
+$(function () {
+    // Owl Carousel
+    var owl = $(".owl-carousel");
+    owl.owlCarousel({
+        items: 4,
+        margin: 10,
+        loop: true,
+        nav: true
+
+    });
+});
+
+// $(function () {
+//     // Owl Carousel
+//     var owl = $(".owl-carousel-1");
+//     owl.owlCarousel({
+//         items: 1,
+//         margin: 10,
+//         loop: true,
+//         nav: true
+//
+//     });
+// });
